98ff8914fc133c98e488de704bd70e31577b7378
Helena Wu <hw87@duke.edu>
2012-03-05 04:03:42 -0500

    added Main.java for GUI

--- src/general/Main.java
+++ src/general/Main.java
@@ -1,10 +1,25 @@
 package general;
 
+
+import java.io.IOException;
+
+import javax.swing.JFrame;
+
 import org.jaxen.JaxenException;
+import GUI.*;
 
 public class Main {
-	public static void main (String[] args) throws JaxenException{
-		TivooSystem.run();
-	}
+	public static void main (String[] args) throws JaxenException, IOException{
+
+		Viewer display = new Viewer();
+
+		JFrame frame = new JFrame("Tivoo!");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        frame.getContentPane().add(display);
+        frame.pack();
+
+        frame.setVisible(true);	
+        }
 
 }
